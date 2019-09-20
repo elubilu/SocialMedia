7a12725af4cc6f24427a38bdbdf49b052229befd
Rasel Ahmed <42867733+raselportfollio@users.noreply.github.com>
2019-09-20 17:33:26 +0600

    contact page

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Socian</title>
+  <!-- site favicon image link -->
+  <link rel="shortcut icon" type="image/png" href="/images/favicon.jpg"/>
+  <!-- main style css file link -->
+  <link rel="stylesheet" href="/css/main.css">
+</head>
+<body>
+
+  <!-- preloader start -->
+  <div id="preloader"></div>
+  <!-- preloader end -->
+
+  <!--  header-section start  -->
+  <header class="header-section">
+    <div class="header-bottom">
+      <div class="container">
+        <nav class="navbar navbar-expand-xl p-0 align-items-start">
+          <a class="site-logo site-title" href="home-one.html"><img src="/images/logo.png" alt="site-logo"><span class="logo-icon"><i class="flaticon-fire"></i></span></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="menu-toggle"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav main-menu ml-auto">
+              
+                <li><a href="#">Home</a> </li>             
+                <li><a href="#">About</a></li>
+                 <li><a href="#">Service</a></li>
+           
+                <li><a href="#">contact us</a></li>
+<!--
+              <li class="menu_has_children"><a href="#0">services</a>
+                <ul class="sub-menu">
+                  <li><a href="service-one.html">service one</a></li>
+                  <li><a href="service-two.html">service two</a></li>
+                  <li><a href="service-details.html">service details</a></li>
+                </ul>
+              </li>
+              <li class="menu_has_children"><a href="#0">projects</a>
+                <ul class="sub-menu">
+                  <li><a href="project-one.html">project one</a></li>
+                  <li><a href="project-two.html">project two</a></li>
+                  <li><a href="project-details.html">project details</a></li>
+                </ul>
+              </li>
+              <li class="menu_has_children"><a href="#0">pages</a>
+                <ul class="sub-menu">
+                  <li><a href="login.html">login</a></li>
+                  <li><a href="registration.html">registration</a></li>
+                  <li><a href="error-404.html">Error-404</a></li>
+                </ul>
+              </li>
+              <li class="menu_has_children"><a href="#0">blog</a>
+                <ul class="sub-menu">
+                  <li><a href="blog-one.html">Blog Style One</a></li>
+                  <li><a href="blog-two.html">Blog Style Two</a></li>
+                  <li><a href="blog-details.html">blog details</a></li>
+                </ul>
+              </li>
+-->
+             
+            </ul>
+            <div class="nav-right">
+              <button class="header-serch-btn toggle-close"><i class="fa fa-search"></i></button>
+              <a href="service-one.html" class="btn btn-secondary btn-radius text-black">get started</a>
+              <div class="header-top-search-area">
+                <form class="header-search-form">
+                  <input type="search" name="header_search" id="header_search" placeholder="Search here...">
+                  <button class="header-search-btn"><i class="fa fa-search"></i></button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div><!-- header-bottom end -->
+  </header>
+  <!--  header-section end  -->
+  
+  <!-- inner-banner-section start -->
+  <section class="inner-banner-section">
+    <div class="inner-banner-el"><img src="assets/images/elements/banner-line-el2.png" alt="image"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="inner-banner-content text-center">
+            <h2 class="page-title">contact with us</h2>
+            <ol class="page-list">
+              <li><a href="home-one.html"><i class="fa fa-home"></i> Home</a></li>
+              <li>contact us</li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- inner-banner-section start -->
+
+  <!-- contact-section start -->
+  <section class="contact-section pt-120 pb-120">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8">
+          <div id="map"></div>
+        </div>
+        <div class="col-lg-4">
+          <div class="contact-item">
+            <h4 class="title">office address</h4>
+            <p>4920 Northwest 21st Avenue Medford, MN 55049 kigkong,USA</p>
+          </div><!-- contact-item end -->
+          <div class="contact-item">
+            <h4 class="title">phone number</h4>
+            <p>+888-555-333-221 , 096545245 878454545 , 45784521 02-3254789645</p>
+          </div><!-- contact-item end -->
+          <div class="contact-item">
+            <h4 class="title">email address</h4>
+            <p>www.renten450info@gmil.com www.rentensupport.com facebook/renten.net</p>
+          </div><!-- contact-item end -->
+        </div>
+        <div class="col-lg-12">
+          <div class="contact-form-area">
+            <h3 class="block-title">send your messages</h3>
+            <form class="cmn-form contact-form">
+              <div class="form-row">
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <input type="text" name="contact_name" id="contact_name" placeholder="Name*" required>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <input type="email" name="contact_phone" id="contact_phone" placeholder="Email*" required>
+                  </div>
+                </div>
+              </div>
+              <div class="form-group">
+                <textarea placeholder="Write Messages" spellcheck="false"></textarea>
+              </div>
+              <button type="submit" class="btn main-bg-one btn-radius">send message</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- contact-section end -->
+
+  <!-- footer-section start -->
+  <footer class="footer-section">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row mb-none-30">
+          <div class="col-xl-4 col-lg-6 col-md-8">
+            <div class="widget footer-widget mb-30">
+              <h4 class="widget-title">about comapny</h4>
+              <div class="widget-body">
+                <p>Lorem ipsum dolor sit amet, sed congufaubus felis quam pellentesque ante suscipipntesque aenean taciti, quis pellentesque adipiscinmpor nibh tortor et mauris scelerisque libero ligula sed erat suspendisse.</p>
+                <a class="phone-num" href="tel:(0161)3478854">Call  (0161) 347 8854</a>
+                <ul class="social-media-links">
+                  <li><a href="#0"><i class="fa fa-facebook-f"></i></a></li>
+                  <li><a href="#0"><i class="fa fa-pinterest-p"></i></a></li>
+                  <li><a href="#0"><i class="fa fa-twitter"></i></a></li>
+                  <li><a href="#0"><i class="fa fa-vimeo"></i></a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6">
+            <div class="widget footer-widget mb-30">
+              <h4 class="widget-title">useful link</h4>
+              <div class="widget-body">
+                <ul class="menu-list">
+                  <li><a href="#0">about</a></li>
+                  <li><a href="#0">services</a></li>
+                  <li><a href="#0">privacy & tream</a></li>
+                  <li><a href="#0">blog post</a></li>
+                  <li><a href="#0">about</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6">
+            <div class="widget footer-widget mb-30">
+              <h4 class="widget-title">our services</h4>
+              <div class="widget-body">
+                <ul class="menu-list">
+                  <li><a href="#0">facebook like</a></li>
+                  <li><a href="#0">youtube subscribe</a></li>
+                  <li><a href="#0">additinal work</a></li>
+                  <li><a href="#0">instagram like</a></li>
+                  <li><a href="#0">media research</a></li>
+                  <li><a href="#0">email marketing</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-xl-4 col-lg-12 col-md-8">
+            <div class="widget footer-widget mb-30">
+              <h4 class="widget-title">contact with us</h4>
+              <div class="widget-body">
+                <form class="contact-form">
+                  <div class="form-group">
+                    <input type="text" name="con_name" id="con_name" placeholder="Name">
+                  </div>
+                  <div class="form-group">
+                    <input type="email" name="con_email" id="con_email" placeholder="Email">
+                  </div>
+                  <div class="form-group">
+                    <textarea placeholder="Message"></textarea>
+                  </div>
+                  <button type="submit" class="submit-btn btn btn-primary">Send Message</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer-bottom">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 col-sm-8 text-sm-left text-center">
+            <p>Copyright Socian© 2019. All rights reserved</p>
+          </div>
+          <div class="col-lg-6 col-sm-4 text-sm-right text-center">
+            <a class="privacy" href="#0">Privacy & Terms</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- footer-section end -->
+
+  <!-- scroll-to-top start -->
+  <div class="scroll-to-top">
+    <span class="scroll-icon">
+      <i class="fa fa-rocket"></i>
+    </span>
+  </div>
+  <!-- scroll-to-top end -->  
+
+  <!-- goolg-map-activate js link -->
+  <script src="/js/goolg-map-activate.js"></script>
+  <!-- main js file -->
+  <script src="/js/main.js"></script>
+</body>
+
+</html>
